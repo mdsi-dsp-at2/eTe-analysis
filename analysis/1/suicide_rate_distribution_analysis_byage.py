c1fac2fae8ad825174fb24fd220a2bc0c4e043e6
HninPwint <hninptgmail.com>
2020-10-27 22:58:45 +1100

    Refactor the code to use common data loader class

--- analysis/1/suicide_rate_distribution_analysis_byage.py
+++ analysis/1/suicide_rate_distribution_analysis_byage.py
@@ -1,32 +1,33 @@
 # This analysis is to investigate the suicide rate distribution by Age
 # The main visualisation feature is Seaborn FacetGrid
+
+# ========= Import the packages =========
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
 
-# Importing customised functions which handling data wrangling
-import common_functions as cf
+# Importing Customised Common Data Reader Class
+sys.path.append(os.path.abspath('..'))
+from common_lib.data_reader import SuicideDataReader, SuicideProcessedData
 
 # ========= Getting Data =========
-suicide_rate_data_filepath = os.path.join("..","..","processed_data", "crude_suicide_rates.csv")
-suicide_rate_dataframe = pd.read_csv(suicide_rate_data_filepath, index_col=0)
-print(suicide_rate_dataframe.head())
 
-# ========= Prepare Data =========
-# Calculate the total suicide rate of all different ages
-suicide_rate_dataframe = cf.find_all_age_suicide_rate(suicide_rate_dataframe)
-#print(suicide_rate_dataframe.head())
+# Use common Data Reader Class to load data
+data_reader = SuicideDataReader()
 
-# Do Melting - Tranform/Combine the multiple  column names of different age groups into one column "Age"
-suicide_rate_pivot_longer_df = cf.pivot_longer_age_columns_to_one(suicide_rate_dataframe)
-#print(suicide_rate_pivot_longer_df)
+# Load Suicide Rate Tiday data done with Pivot Longer the rate of all age ranges
+suicide_rate_dataframe = data_reader.read_data(SuicideProcessedData.SUICIDE_RATES, tidy=True)
+# print(suicide_rate_dataframe.head(10))
+
 
 # ========= plotting graph =========
+
 # Plotting the suicide rate distribution in FacetGrid
 sns.set_style("dark")
-g = sns.FacetGrid(suicide_rate_pivot_longer_df, col="Age", col_wrap=3)
-g.map_dataframe(plt.hist, x="Suicide_rate")
+g = sns.FacetGrid(suicide_rate_dataframe, col="age_range", col_wrap=4)
+g.map_dataframe(plt.hist, x="suicide_rate")
 g.set_axis_labels("", "Suicide Rate")
 # This is to adjust the axis and display the main title
 # without it, seaborn"s facet titles and the main title are overlapped
